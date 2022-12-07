f79dac8fb0da10f01ee58f7436473ecb1aba5a4d
Andrei Nascimento <andrei.lima.nas@outlook.com>
2022-12-06 21:19:16 -0300

    Ajuste de layout

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   return (
     <div className="App">
       <textarea
-      placeholder="Digite aí"
+      placeholder="Digite e veja o resultado"
       rows={15}
       value={codigo}
       onChange={(e) => setCodigo(e.target.value)}>
       </textarea>
       
       <div className="mirror">
-        <h1>Código</h1>
+        <h1>Código Transformado</h1>
         {codigo.toUpperCase()}
       </div>
     </div >
